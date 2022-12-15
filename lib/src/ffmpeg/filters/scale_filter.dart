55f6269fe37ae04859fd75307911a826936ab331
Abhay S Rawat <abhayrawat.official@gmail.com>
2022-12-16 02:46:27 +0530

    ffmpeg changes

--- lib/src/ffmpeg/filters/scale_filter.dart
+++ lib/src/ffmpeg/filters/scale_filter.dart
@@ -9,18 +9,15 @@ class ScaleFilter implements Filter {
     this.interl,
     this.param0,
     this.param1,
-    this.size,
-  })  : assert(width == null || width >= -1),
-        assert(height == null || height >= -1);
+  });
 
-  final int? width;
-  final int? height;
+  final String? width;
+  final String? height;
   final String? eval;
   final int? interl;
   // TODO: flags
   final String? param0;
   final String? param1;
-  final VideoSize? size;
   // TODO: in_color_matrix
   // TODO: out_color_matrix
   // TODO: in_range
@@ -37,7 +34,6 @@ class ScaleFilter implements Filter {
       if (interl != null) 'interl=$interl',
       if (param0 != null) 'param0=$param0',
       if (param1 != null) 'param1=$param1',
-      if (size != null) 'size=${size.toString()}',
     ];
     if (properties.isNotEmpty) {
       return 'scale=${properties.join(':')}';
