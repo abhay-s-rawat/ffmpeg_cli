bcc8f4108a60040608fbe52b5d6d0f31f9eb3e97
Abhay S Rawat <abhayrawat.official@gmail.com>
2022-12-16 02:36:56 +0530

    ffmpeg changes

--- lib/src/ffmpeg/video_size.dart
+++ lib/src/ffmpeg/video_size.dart
@@ -8,12 +8,15 @@ class VideoSize {
   final num height;
 
   @override
-  String toString() => '[Size]: ${width}x$height';
+  String toString() => '${width}x$height';
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is VideoSize && runtimeType == other.runtimeType && width == other.width && height == other.height;
+      other is VideoSize &&
+          runtimeType == other.runtimeType &&
+          width == other.width &&
+          height == other.height;
 
   @override
   int get hashCode => width.hashCode ^ height.hashCode;
